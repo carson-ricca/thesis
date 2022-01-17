499cefdf19c3ad6fbe984205aa6954c08557f96c
Carson Ricca <59884796+carson-ricca@users.noreply.github.com>
2022-01-17 15:27:57 -0800

    Add Initial Testing State to Parent Category Bayes Net

--- test_models/test_parent_categories.py
+++ test_models/test_parent_categories.py
@@ -3,10 +3,10 @@ from modelling import generate_parent_categories_bayesian_network
 
 def test_parent_categories():
     model = generate_parent_categories_bayesian_network()
-    _make_prediction(model, 'Success', 'Success', 'Success', 'Success', 'Success', 'Success', 'Success')
+    _make_prediction(model, 'Success', 'Success', 'Success', 'Success', 'Success', 'Success')
 
 
-def _make_prediction(model, basics, conditionals, pre_defined_classes, loops, arrays, methods, oop):
+def _make_prediction(model, basics, conditionals, pre_defined_classes, loops, arrays, methods):
     predictions = model.predict_proba({
         'Basics': basics,
         'Conditionals': conditionals,
@@ -14,13 +14,12 @@ def _make_prediction(model, basics, conditionals, pre_defined_classes, loops, ar
         'Loops': loops,
         'Arrays': arrays,
         'Methods': methods,
-        'OOP': oop
     })
-    category_success = predictions[0].parameters[0]
+    category_success = predictions[6].parameters[0]
     success = category_success['Success']
     failure = category_success['Failure']
     print(
-        f'Basics: {basics}, Conditionals: {conditionals}, Pre-Defined Classes: {pre_defined_classes}, Loops: {loops}, Arrays: {arrays}, Methods: {methods}, OOP: {oop}')
+        f'Basics: {basics}, Conditionals: {conditionals}, Pre-Defined Classes: {pre_defined_classes}, Loops: {loops}, Arrays: {arrays}, Methods: {methods}')
     print(f'Success: {success}, Failure: {failure}')
     print('-' * 150)
 
