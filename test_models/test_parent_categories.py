d760648e30e016f39b076f1ef721175aa528d68c
Carson Ricca <59884796+carson-ricca@users.noreply.github.com>
2022-01-26 14:35:18 -0800

    Update test_parent_categories.py

--- test_models/test_parent_categories.py
+++ test_models/test_parent_categories.py
@@ -5,6 +5,11 @@ from constants import success, failure
 def test_parent_categories():
     model = generate_parent_categories_bayesian_network()
     _make_prediction(model, success, success, success, success, success, success)
+    _make_prediction(model, failure, failure, failure, failure, failure, failure)
+    _make_prediction(model, failure, failure, failure, success, success, success)
+    _make_prediction(model, success, success, success, failure, failure, failure)
+    _make_prediction(model, success, failure, success, failure, success, failure)
+    _make_prediction(model, failure, success, success, success, success, failure)
 
 
 def _make_prediction(model, basics, conditionals, pre_defined_classes, loops, arrays, methods):
@@ -19,9 +24,11 @@ def _make_prediction(model, basics, conditionals, pre_defined_classes, loops, ar
     category_success = predictions[6].parameters[0]
     probable_success = category_success[success]
     probable_failure = category_success[failure]
-    print(
-        f'Basics: {basics}, Conditionals: {conditionals}, Pre-Defined Classes: {pre_defined_classes}, Loops: {loops}, Arrays: {arrays}, Methods: {methods}')
-    print(f'Success: {probable_success}, Failure: {probable_failure}')
+    print((
+        f'Basics: {basics}, Conditionals: {conditionals}, Pre-Defined Classes: {pre_defined_classes},\nLoops: {loops}, '
+        f'Arrays: {arrays}, Methods: {methods}'
+    ))
+    print(f'OOP Success: {probable_success}, OOP Failure: {probable_failure}')
     print('-' * 150)
 
 
