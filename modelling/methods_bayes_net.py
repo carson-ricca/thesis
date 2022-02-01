24c1bd48308a49f84fa8b2bbe84c555b1b8e0344
Carson Ricca <59884796+carson-ricca@users.noreply.github.com>
2022-02-01 13:23:48 -0800

    Methods DocString

--- modelling/methods_bayes_net.py
+++ modelling/methods_bayes_net.py
@@ -4,6 +4,10 @@ from constants import success, failure
 
 
 def generate_methods_bayesian_network():
+    """
+    Creates the Bayesian Network for the Methods sub-category.
+    :return: The complete Bayesian Network.
+    """
     methods = _get_methods_probability()
     abstraction = _get_abstraction_probability(methods)
     variable_scope = _get_variable_scope_probability(methods)
@@ -41,10 +45,19 @@ def generate_methods_bayesian_network():
 
 
 def _get_methods_probability():
+    """
+    Gets the probability of the Methods node.
+    :return: The probability of the Methods node.
+    """
     return DiscreteDistribution({success: 0.5, failure: 0.5})
 
 
 def _get_abstraction_probability(methods):
+    """
+    Gets the probability of the Abstraction node.
+    :param methods: The probability of the Methods node.
+    :return: The probability of the Abstraction node.
+    """
     return ConditionalProbabilityTable([
         [success, success, 0.9],
         [success, failure, 0.1],
@@ -54,6 +67,11 @@ def _get_abstraction_probability(methods):
 
 
 def _get_variable_scope_probability(methods):
+    """
+    Gets the probability of the Variable Scope node.
+    :param methods: The probability of the Methods node.
+    :return: The probability of the Variable Scope node.
+    """
     return ConditionalProbabilityTable([
         [success, success, 0.9],
         [success, failure, 0.1],
@@ -63,6 +81,12 @@ def _get_variable_scope_probability(methods):
 
 
 def _get_using_methods_probability(methods, abstraction):
+    """
+    Gets the probability of the Using Methods node.
+    :param methods: The probability of the Methods node.
+    :param abstraction: The probability of the Abstraction node.
+    :return: The probability of the Using Methods node.
+    """
     return ConditionalProbabilityTable([
         [success, success, success, 0.9],
         [success, success, failure, 0.1],
@@ -76,6 +100,14 @@ def _get_using_methods_probability(methods, abstraction):
 
 
 def _get_defining_methods_probability(methods, abstraction, variable_scope, using_methods):
+    """
+    Gets the probability of the Defining Methods node.
+    :param methods: The probability of the Methods node.
+    :param abstraction: The probability of the Abstraction node.
+    :param variable_scope: The probability of the Variable Scope node.
+    :param using_methods: The probability of the Using Methods node.
+    :return: The probability of the Defining Methods node.
+    """
     return ConditionalProbabilityTable([
         [success, success, success, success, success, 0.95],
         [success, success, success, success, failure, 0.05],
@@ -113,6 +145,12 @@ def _get_defining_methods_probability(methods, abstraction, variable_scope, usin
 
 
 def _get_method_overloading_probability(methods, defining_methods):
+    """
+    Gets the probability of the Method Overloading node.
+    :param methods: The probability of the Methods node.
+    :param defining_methods: The probability of the Defining Methods node.
+    :return: The probability of the Method Overloading node.
+    """
     return ConditionalProbabilityTable([
         [success, success, success, 0.9],
         [success, success, failure, 0.1],
@@ -126,6 +164,13 @@ def _get_method_overloading_probability(methods, defining_methods):
 
 
 def _get_modular_programs_probability(methods, defining_methods, method_overloading):
+    """
+    Gets the probability of the Modular Programs node.
+    :param methods: The probability of the Methods node.
+    :param defining_methods: The probability of the Defining Methods node.
+    :param method_overloading: The probability of the Method Overloading node.
+    :return: The probability of the Modular Programs node.
+    """
     return ConditionalProbabilityTable([
         [success, success, success, success, 0.9],
         [success, success, success, failure, 0.1],
