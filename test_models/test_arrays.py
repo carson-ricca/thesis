43ff0e106f11c0c55216c7bcec3136f8d7451f5f
Carson Ricca <59884796+carson-ricca@users.noreply.github.com>
2022-02-03 16:23:41 -0800

    Add More Array Tests

--- test_models/test_arrays.py
+++ test_models/test_arrays.py
@@ -4,6 +4,17 @@ from constants import success, failure
 from modelling import generate_arrays_bayesian_network
 from util import Timer
 
+NODE_ORDER = {
+    'Arrays': 0,
+    'Data Representation': 1,
+    'Defining Arrays': 2,
+    'Referencing Arrays': 3,
+    'Multidimensional Arrays': 4,
+    'Array with Methods': 5,
+    'Programs with Data Sequences': 6,
+    'Programs with Multidimensional Data': 7
+}
+
 
 def test_arrays():
     model = generate_arrays_bayesian_network()
@@ -17,6 +28,52 @@ def test_arrays():
     _predict_success_in_arrays(model, success, failure, success, failure, success, failure, success)
     _predict_success_in_arrays(model, failure, failure, success, success, success, failure, failure)
     _predict_success_in_arrays(model, failure, success, success, success, success, success, failure)
+
+    print('Programs with Multidimensional Data')
+    _run_inference(model, {
+        'Arrays': success,
+        'Data Representation': success,
+        'Defining Arrays': success,
+        'Referencing Arrays': success,
+        'Multidimensional Arrays': success,
+        'Array with Methods': failure,
+        'Programs with Data Sequences': failure,
+    }, 'Programs with Multidimensional Data')
+
+    print('Data Representation')
+    _run_inference(model, {
+        'Arrays': success,
+    }, 'Data Representation')
+
+    print('Array with Methods')
+    _run_inference(model, {
+        'Arrays': success,
+        'Data Representation': success,
+        'Defining Arrays': failure,
+        'Referencing Arrays': failure,
+        'Multidimensional Arrays': success,
+        'Programs with Data Sequences': success,
+        'Programs with Multidimensional Data': success
+    }, 'Array with Methods')
+
+    print('Programs with Data Sequences')
+    _run_inference(model, {
+        'Arrays': success,
+        'Data Representation': failure,
+        'Defining Arrays': failure,
+        'Referencing Arrays': failure,
+        'Multidimensional Arrays': success,
+        'Array with Methods': failure,
+    }, 'Programs with Data Sequences')
+
+
+def _run_inference(model, data, estimated_node):
+    timer = Timer()
+    timer.start()
+    predictions = model.predict_proba(data)
+    timer.stop()
+    print(predictions[NODE_ORDER.get(estimated_node)].parameters[0][success])
+    print('-' * 150)
 
 
 def _predict_success_in_arrays(model, data_representation, defining_arrays, referencing_arrays,
