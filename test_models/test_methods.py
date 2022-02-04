9c2ca36368fdb03be8922408b7fb1b63f3fcc6a2
Carson Ricca <59884796+carson-ricca@users.noreply.github.com>
2022-02-04 15:40:31 -0800

    Update test_methods.py

--- test_models/test_methods.py
+++ test_models/test_methods.py
@@ -4,6 +4,16 @@ from constants import success, failure
 from modelling import generate_methods_bayesian_network
 from util import Timer
 
+NODE_ORDER = {
+    'Methods': 0,
+    'Abstraction': 1,
+    'Variable Scope': 2,
+    'Using Methods': 3,
+    'Defining Methods': 4,
+    'Method Overloading': 5,
+    'Modular Programs': 6
+}
+
 
 def test_methods():
     model = generate_methods_bayesian_network()
@@ -16,6 +26,52 @@ def test_methods():
     _predict_success_in_loops(model, success, success, success, failure, failure, failure)
     _predict_success_in_loops(model, failure, success, failure, success, failure, success)
     _predict_success_in_loops(model, failure, success, success, success, success, failure)
+
+    print('Methods')
+    _run_inference(model, {
+        'Abstraction': success,
+        'Variable Scope': success,
+        'Using Methods': success,
+        'Defining Methods': success,
+        'Method Overloading': success,
+        'Modular Programs': failure,
+    }, 'Methods')
+
+    print('Methods')
+    _run_inference(model, {
+        'Abstraction': failure,
+        'Variable Scope': failure,
+        'Using Methods': failure,
+        'Defining Methods': failure,
+        'Method Overloading': failure,
+        'Modular Programs': failure,
+    }, 'Methods')
+
+    print('Modular Programs')
+    _run_inference(model, {
+        'Using Methods': success,
+        'Defining Methods': failure,
+        'Method Overloading': success,
+    }, 'Modular Programs')
+
+    print('Defining Methods')
+    _run_inference(model, {
+        'Methods': failure,
+        'Abstraction': success,
+        'Variable Scope': failure,
+        'Using Methods': success,
+        'Method Overloading': failure,
+        'Modular Programs': failure,
+    }, 'Defining Methods')
+
+
+def _run_inference(model, data, estimated_node):
+    timer = Timer()
+    timer.start()
+    predictions = model.predict_proba(data)
+    timer.stop()
+    print(predictions[NODE_ORDER.get(estimated_node)].parameters[0][success])
+    print('-' * 150)
 
 
 def _predict_success_in_loops(model, abstraction, variable_scope, using_methods, defining_methods, method_overloading,
