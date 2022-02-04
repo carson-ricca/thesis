4386c6aea5ab680294de49228d8bc64fe84e5fa7
Carson Ricca <59884796+carson-ricca@users.noreply.github.com>
2022-02-03 16:59:55 -0800

    Add More Conditionals Tests

--- test_models/test_conditionals.py
+++ test_models/test_conditionals.py
@@ -2,6 +2,17 @@ from modelling import generate_conditionals_bayesian_network
 from constants import success, failure
 from util import Timer
 
+NODE_ORDER = {
+    'Conditionals': 0,
+    'Boolean': 1,
+    'Decision': 2,
+    'Operators': 3,
+    'Conditionals Statements': 4,
+    'Nested Conditional Statements': 5,
+    'Simple Programs': 6,
+    'Programs': 7
+}
+
 
 def test_conditionals():
     model = generate_conditionals_bayesian_network()
@@ -17,6 +28,52 @@ def test_conditionals():
     _predict_success_in_conditionals(model, success, success, success, failure, failure, failure, failure)
     # Test more failure than success (easier topics).
     _predict_success_in_conditionals(model, failure, failure, failure, failure, success, success, success)
+
+    print('Conditionals')
+    _run_inference(model, {
+        'Boolean': success,
+        'Decision': success,
+        'Operators': success,
+        'Conditional Statements': success,
+        'Nested Conditional Statements': success,
+        'Simple Programs': success,
+        'Programs': success
+    }, 'Conditionals')
+
+    print('Conditionals')
+    _run_inference(model, {
+        'Boolean': failure,
+        'Decision': failure,
+        'Operators': failure,
+        'Conditional Statements': success,
+        'Nested Conditional Statements': success,
+    }, 'Conditionals')
+
+    print('Programs')
+    _run_inference(model, {
+        'Conditionals': success,
+        'Boolean': success,
+    }, 'Programs')
+
+    print('Operators')
+    _run_inference(model, {
+        'Conditionals': success,
+        'Boolean': failure,
+        'Decision': success,
+        'Conditional Statements': success,
+        'Nested Conditional Statements': success,
+        'Simple Programs': success,
+        'Programs': failure
+    }, 'Operators')
+
+
+def _run_inference(model, data, estimated_node):
+    timer = Timer()
+    timer.start()
+    predictions = model.predict_proba(data)
+    timer.stop()
+    print(predictions[NODE_ORDER.get(estimated_node)].parameters[0][success])
+    print('-' * 150)
 
 
 def _predict_success_in_conditionals(model, boolean, decision, operators, conditional_statements,
