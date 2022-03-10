67284eff75837dfcb1d6f01e4e42f3963f2d5af5
Carson Ricca <59884796+carson-ricca@users.noreply.github.com>
2022-03-10 14:39:15 -0800

    Update performance_dbn.py
    - Change the performance to be a DBN.

--- dbn_modelling/performance_dbn.py
+++ dbn_modelling/performance_dbn.py
@@ -1,11 +1,15 @@
 from pgmpy.factors.discrete import TabularCPD
-from pgmpy.inference import BeliefPropagation
-from pgmpy.models import BayesianNetwork
+from pgmpy.inference import DBNInference
+from pgmpy.models import DynamicBayesianNetwork
 
-performance_node = 'Performance'
-correctness_node = 'Correctness'
-skipped_question_node = 'Skipped Question'
-time_taken_node = 'Time Taken'
+performance_node_0 = ('Performance', 0)
+correctness_node_0 = ('Correctness', 0)
+skipped_question_node_0 = ('Skipped Question', 0)
+time_taken_node_0 = ('Time Taken', 0)
+performance_node_1 = ('Performance', 1)
+correctness_node_1 = ('Correctness', 1)
+skipped_question_node_1 = ('Skipped Question', 1)
+time_taken_node_1 = ('Time Taken', 1)
 
 
 def performance_dbn():
@@ -19,77 +23,49 @@ def performance_dbn():
     Time Taken: Long - 0, Medium - 1, Short - 2
     :return: The generated model.
     """
-    model = BayesianNetwork([
-        (performance_node, correctness_node),
-        (performance_node, skipped_question_node),
-        (performance_node, time_taken_node)
+    model = DynamicBayesianNetwork([
+        (performance_node_0, correctness_node_0),
+        (performance_node_0, skipped_question_node_0),
+        (performance_node_0, time_taken_node_0),
+        (performance_node_0, performance_node_1),
+        (performance_node_1, correctness_node_1),
+        (performance_node_1, skipped_question_node_1),
+        (performance_node_1, time_taken_node_1),
     ])
 
-    performance_cpd = TabularCPD(performance_node, 2, [
+    performance_cpd = TabularCPD(performance_node_0, 2, [
         [0.5], [0.5]
     ])
 
-    correctness_cpd = TabularCPD(correctness_node, 2, [
+    correctness_cpd = TabularCPD(correctness_node_0, 2, [
         [0.9, 0.2],
         [0.1, 0.8]
-    ], evidence=[performance_node], evidence_card=[2])
+    ], evidence=[performance_node_0], evidence_card=[2])
 
-    skipped_question_cpd = TabularCPD(skipped_question_node, 2, [
+    skipped_question_cpd = TabularCPD(skipped_question_node_0, 2, [
         [0.1, 0.7],
         [0.9, 0.3]
-    ], evidence=[performance_node], evidence_card=[2])
+    ], evidence=[performance_node_0], evidence_card=[2])
 
-    time_taken_cpd = TabularCPD(time_taken_node, 3, [
+    time_taken_cpd = TabularCPD(time_taken_node_0, 3, [
         [0.1, 0.4],
         [0.3, 0.4],
         [0.6, 0.2]
-    ], evidence=[performance_node], evidence_card=[2])
+    ], evidence=[performance_node_0], evidence_card=[2])
 
-    model.add_cpds(performance_cpd, correctness_cpd, skipped_question_cpd, time_taken_cpd)
+    performance_transitional_cpd = TabularCPD(performance_node_1, 2, [
+        [0.8, 0.2],
+        [0.2, 0.8]
+    ], evidence=[performance_node_0], evidence_card=[2])
+
+    model.add_cpds(performance_cpd, correctness_cpd, skipped_question_cpd, time_taken_cpd, performance_transitional_cpd)
+    model.initialize_initial_state()
+    model.check_model()
     return model
 
 
 def test_inference(model):
-    bp = BeliefPropagation(model)
-
-    # Correct, not skipped, short time.
-    print(bp.query([performance_node], {
-        correctness_node: 0,
-        skipped_question_node: 1,
-        time_taken_node: 2
-    }))
-
-    # Skipped, short time.
-    print(bp.query([performance_node], {
-        skipped_question_node: 0,
-        time_taken_node: 2
-    }))
-
-    # Correct, not skipped, long time.
-    print(bp.query([performance_node], {
-        correctness_node: 0,
-        skipped_question_node: 1,
-        time_taken_node: 0
-    }))
-
-    # Incorrect, not skipped, short time.
-    print(bp.query([performance_node], {
-        correctness_node: 1,
-        skipped_question_node: 1,
-        time_taken_node: 2
-    }))
-
-    # Correct, not skipped, medium time.
-    print(bp.query([performance_node], {
-        correctness_node: 0,
-        skipped_question_node: 1,
-        time_taken_node: 1
-    }))
-
-    # Correct.
-    print(bp.query([performance_node], {
-        correctness_node: 0
-    }))
+    dbn_inf = DBNInference(model)
 
 
 if __name__ == '__main__':
